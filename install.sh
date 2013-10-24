7e4b52125f6193e5a70494b91402e2c819c2a43d
Andreas Moregard Haubenwaller <andreas.moregard@gmail.com>
2013-10-24 16:10:59 +0200

    Added sudo to the script file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
-add-apt-repository "deb http://ftp.sunet.se/pub/lang/CRAN/bin/linux/ubuntu precise/" 
-apt-get update
-apt-get install r-base
-wget http://cran.r-project.org/src/contrib/forecast_4.8.tar.gz
-R CMD INSTALL forecast_4.8.tar.gz
+sudo add-apt-repository "deb http://ftp.sunet.se/pub/lang/CRAN/bin/linux/ubuntu precise/" 
+sudo apt-get update
+sudo apt-get install r-base
+sudo wget http://cran.r-project.org/src/contrib/forecast_4.8.tar.gz
+sudo R CMD INSTALL forecast_4.8.tar.gz
 
